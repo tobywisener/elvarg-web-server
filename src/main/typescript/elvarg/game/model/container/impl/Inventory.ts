a69c8c4fbecd4fda8d7cbd12505acfd0f2ec969c
Toby Wisener <toby.wisener@commscope.com>
2023-04-12 12:37:55 +0100

    Compile working

--- src/main/typescript/elvarg/game/model/container/impl/Inventory.ts
+++ src/main/typescript/elvarg/game/model/container/impl/Inventory.ts
@@ -6,24 +6,24 @@ import { StackType } from "../StackType";
 export class Inventory extends ItemContainer {
 
     public static readonly INTERFACE_ID: number = 3214;
-    
+
     constructor(player: Player) {
         super(player);
     }
-    
+
     public capacity(): number {
         return 28;
     }
-    
+
     public stackType(): StackType {
         return StackType.DEFAULT;
     }
-    
+
     public refreshItems(): ItemContainer {
         this.getPlayer().getPacketSender().sendItemContainer(this, Inventory.INTERFACE_ID);
-        return this.refreshItems();
+        return this;
     }
-    
+
     public full(itemId?: number): ItemContainer {
         this.getPlayer().getPacketSender().sendMessage("Not enough space in your inventory.");
         return this;
